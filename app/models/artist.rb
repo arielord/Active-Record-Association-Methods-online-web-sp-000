9a3e2863e562aad1e3bd776754b250d0b2c33e3a
Ariel <arielord@buffalo.edu>
2019-03-17 03:07:54 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,11 +9,11 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    self.song.count
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
-    self.genre.length
+    self.genres.length
   end
 end
